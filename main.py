3125c6d390342cc09d734af89d36ab80b1e04005
hermangavrylov <gavrylovherman@gmail.com>
2025-05-06 03:01:15 +0200

    Add automatic file downloading

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from psswd import USERNAME, PASSWORD
+import os
 
 chrome_options = Options()
 chrome_options.add_argument("--disable--search-engine-choice-screen")
+
+download_path = os.getcwd()
+prefs = {'download.default_directory': download_path}
+chrome_options.add_experimental_option('prefs', prefs)
+
 service = Service('chromedriver-mac-arm64/chromedriver')
 driver = webdriver.Chrome(options=chrome_options, service=service)
 
@@ -50,6 +56,14 @@ email_field.send_keys('smith@gmail.com')
 current_address_field.send_keys('St.Peterson 100, New York, USA')
 permanent_address_field.send_keys('St.Peterson 100, New York, USA')
 driver.execute_script("arguments[0].click();", submit_button)
+
+# Locate the Upload and Download section
+
+upload_download = WebDriverWait(driver, 10).\
+    until(EC.visibility_of_element_located((By.ID, 'item-7')))
+upload_download.click()
+download_button = driver.find_element(By.ID, 'downloadButton')
+driver.execute_script("arguments[0].click();", download_button)
 
 input("Press Enter to close the browser")
 driver.quit()
